981cc2b98e86e2764846bc681033be7240bd18d0
maria prado <145464742+mariaprado000@users.noreply.github.com>
2023-10-17 11:22:18 -0300

    Add files via upload

---
+++ aula01/exp01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>testando php</title>
+</head>
+<body>
+<?php
+echo "testando php em html "
+
+
+?>
+</body>
+</html>
---
+++ aula01/oxp.05.php
@@ -0,0 +1,11 @@
+<?php
+$curso ="Web Médio- aluno 10!";
+$disciplina = "PSW1";
+$turma = 2022;
+
+echo "[CURSO] : $curso <br>";
+echo "[DISCIPLINA] : $disciplina <br>";
+echo "[TURMA] : $turma <br>";
+
+
+?>
